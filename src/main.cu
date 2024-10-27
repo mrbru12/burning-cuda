e526465df88c8471150ba8410837911156f3b26d
mrbru12 <bmmcruzalta2@gmail.com>
2024-10-27 03:03:48 -0300

    generated buning ship BMP

--- src/main.cu
+++ src/main.cu
@@ -10,7 +10,7 @@ typedef uint32_t pixel_t;
 
 __managed__ pixel_t *pixels;
 
-__device__ pixel_t pixel_from_rgb(int r, int g, int b) {
+__device__ pixel_t pixel_from_rgb(uint8_t r, uint8_t g, uint8_t b) {
 	pixel_t pixel;
 	pixel  = b << 0;
 	pixel |= g << 8;
@@ -20,18 +20,34 @@ __device__ pixel_t pixel_from_rgb(int r, int g, int b) {
 	return pixel;
 }
 
-__global__ void fill_pixels(pixel_t *pixels) {
+__global__ void burning_ship_kernel(pixel_t *pixels, int width, int height, int max_iterations) {
 	int x = threadIdx.x + blockIdx.x * blockDim.x;
 	int y = threadIdx.y + blockIdx.y * blockDim.y;
 
-	pixel_t color;
-	if ((threadIdx.x + threadIdx.y) % 8 == 0) {
-		color = pixel_from_rgb(255, 0, 0);
-	} else {
-		color = pixel_from_rgb(0, 0, 0);
+	double zx = 0.0, zy = 0.0;
+
+	// Here is where the magic happens :)
+	double scale = 4.0;
+	double cx = ((double)x - (double)width / 2.0) * scale / (double)width;
+	double cy = ((double)y - (double)height / 2.0) * scale / (double)height;
+
+	int iteration = 0;
+	while (zx * zx + zy * zy < 4.0 && iteration < max_iterations) {
+		double temp = zx * zx - zy * zy + cx;
+
+		zy = abs(2.0 * zx * zy) + cy;
+		zx = temp;
+
+		iteration++;
 	}
-	
-	int width = gridDim.x * blockDim.x;
+
+	pixel_t color;
+	if (iteration == max_iterations) {
+		color = pixel_from_rgb(0, 0, 0);
+	} else {
+		color = pixel_from_rgb(0, (max_iterations / iteration) * 255, 0);
+	}
+
 	pixels[y * width + x] = color;
 }
 
@@ -75,7 +91,7 @@ int main(int argc, char *argv[]) {
 		printf("ERROR: Failed to initialize SDL!\n");
 	}
 
-	int width = 400, height = 400;
+	int width = 2000, height = 2000;
 	// int grid_width = 4, grid_height = 4;
 
 	cudaMallocManaged(&pixels, width * height * sizeof(pixel_t));
@@ -83,9 +99,13 @@ int main(int argc, char *argv[]) {
 	dim3 block_dim, thread_dim;
 	calculate_dims(&block_dim, &thread_dim, width, height);
 
-	fill_pixels<<<block_dim, thread_dim>>>(pixels);
+	printf("Generating fractal...\n");
+
+	burning_ship_kernel<<<block_dim, thread_dim>>>(pixels, width, height, 1000);
 
 	cudaDeviceSynchronize();
+
+	printf("Done!\n");
 
 	SDL_Surface *surface = SDL_CreateRGBSurfaceFrom(
 		pixels, width, height, 32, width * sizeof(pixel_t),
